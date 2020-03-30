4c00bf6e0bfa9e5bcd3cafac26322a91bce10ea5
Vasyl Kovbassa <movasyl@gmail.com>
2020-03-30 09:11:43 +0300

    Add files via upload

--- 3/8.js
+++ 3/8.js
@@ -1,5 +1,7 @@
 // Напишіть функцію, яка приймає рядок як параметр і перетворює
 // першу букву кожного слова рядка в верхній регістр.
 
-// Input string: 'i love java script' 
-// Output string: 'I Love Java Script'
+function upperCase(str) {
+    return str.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+}
+console.log(upperCase('i love java script'));
