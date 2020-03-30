1822edfcc072dc87ca10fe6934de28041d0ed85b
Vasyl Kovbassa <movasyl@gmail.com>
2020-03-30 04:06:11 +0300

    upload 2

--- 2/2.js
+++ 2/2.js
@@ -1,1 +1,14 @@
-// ...
+let num = +prompt('Enter a number');
+
+if (num > 0) {
+    !(num % 2) && !(num % 7) ? console.log(`число парне & кратне 7`)
+        : !(num % 2) ? console.log(`число парне`)
+            : !(num % 7) ? console.log(`число кратне 7`)
+                : console.log(`введене число не відповідає умові`);
+
+
+
+
+} else {
+    console.log(`число від'ємне`);
+}
